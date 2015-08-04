6017cb04daabdc82a012afef76b107ce7767bc6a
ohMaxy <404408395@qq.com>
2015-08-04 19:15:17 +0800

    首次提交

---
+++ src/main/java/me/netty/discard/DiscardServerHandler.java
@@ -0,0 +1,31 @@
+package me.netty.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+//            while (in.isReadable()) { // (1)
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+            System.out.println(in.toString(io.netty.util.CharsetUtil.UTF_8));
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        System.out.println("exceptionCaught...");
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
